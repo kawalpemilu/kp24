335a6bb1d5ad89ffad0c030132fab0ae523a8ee1
Felix Halim <felix.halim@gmail.com>
2024-01-13 17:56:09 +0000

    Add angular material.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,19 +8,20 @@ import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingServ
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getFunctions, provideFunctions } from '@angular/fire/functions';
 import { getStorage, provideStorage } from '@angular/fire/storage';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     importProvidersFrom(provideFirebaseApp(() => initializeApp({
-      "projectId": "kp24-fd486",
-      "appId": "1:887593210576:web:b697f44855185de4f784d5",
-      "databaseURL": "https://kp24-fd486-default-rtdb.asia-southeast1.firebasedatabase.app",
-      "storageBucket": "kp24-fd486.appspot.com",
-      "apiKey": "AIzaSyBbLVuESLAgRiadGXI-Y7nn7SpHivD7sOo",
-      "authDomain": "kp24-fd486.firebaseapp.com",
-      "messagingSenderId": "887593210576",
-      "measurementId": "G-DEEZMKY15N"
+        "projectId": "kp24-fd486",
+        "appId": "1:887593210576:web:b697f44855185de4f784d5",
+        "databaseURL": "https://kp24-fd486-default-rtdb.asia-southeast1.firebasedatabase.app",
+        "storageBucket": "kp24-fd486.appspot.com",
+        "apiKey": "AIzaSyBbLVuESLAgRiadGXI-Y7nn7SpHivD7sOo",
+        "authDomain": "kp24-fd486.firebaseapp.com",
+        "messagingSenderId": "887593210576",
+        "measurementId": "G-DEEZMKY15N"
     }))),
     importProvidersFrom(provideAuth(() => getAuth())),
     importProvidersFrom(provideAnalytics(() => getAnalytics())),
@@ -29,5 +30,6 @@ export const appConfig: ApplicationConfig = {
     importProvidersFrom(provideStorage(() => getStorage())),
     ScreenTrackingService,
     UserTrackingService,
-  ]
+    provideAnimations()
+]
 };
