ce53ec7b298aa875e2bbfa0c510b29943425ad5c
Zain Fathoni <me@zainf.dev>
2024-02-05 23:52:43 +0800

    style(about): add social media links

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,6 +6,7 @@ import { CommonModule } from '@angular/common';
     standalone: true,
     imports: [CommonModule],
     templateUrl: './about.component.html',
+    styleUrl: './about.component.css'
 })
 export class AboutComponent {
 }
