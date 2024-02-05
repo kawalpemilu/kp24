13affde0a0ffb1cd5f473d37646a446ae0e3c035
Felix Halim <felix.halim@gmail.com>
2024-02-04 23:37:10 -0800

    Show uploaded and reviewed photos in user profile.

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -3,27 +3,72 @@ import { CommonModule } from '@angular/common';
 import { AppService } from '../app.service';
 import { FormsModule } from '@angular/forms';
 import { MatRadioModule } from '@angular/material/radio';
-import { Observable } from 'rxjs';
-import { UserProfile } from '../../../functions/src/interfaces';
+import { Observable, combineLatest, of, switchMap } from 'rxjs';
+import { Lokasi, UploadRequest, UserProfile } from '../../../functions/src/interfaces';
+import { PhotoComponent } from '../photo/photo.component';
+import { RouterLink } from '@angular/router';
+
+interface UserUpload {
+    lokasi: Lokasi;
+    uploadRequest: UploadRequest;
+}
+
+interface UserReview {
+    idLokasi: string;
+    lokasi: Lokasi;
+    numReviews: number;
+}
+
+interface ProfileDetails {
+    profile: UserProfile;
+    uploads: UserUpload[];
+    reviews: UserReview[];
+}
 
 @Component({
     selector: 'app-user-profile',
     standalone: true,
-    imports: [CommonModule, FormsModule, MatRadioModule],
+    imports: [CommonModule, FormsModule, MatRadioModule, PhotoComponent, RouterLink],
     templateUrl: './profile.component.html',
 })
 export class UserProfileComponent implements OnInit {
     @Input() uid = '';
 
-    profile$?: Observable<UserProfile | null>;
+    profile$?: Observable<ProfileDetails>;
+    STATUS = ['NEW', 'APPROVED', 'REJECTED', 'MOVED'];
 
-    constructor(private service: AppService) { }
+    constructor(public service: AppService) { }
 
-    ngOnInit() {
-        if (this.uid.length > 0) {
-            this.profile$ = this.service.getUserProfile$(this.uid);
-        } else {
-            this.profile$ = this.service.profile$;
-        }
+    async ngOnInit() {
+        const userProfile$ = (this.uid.length > 0) ?
+            this.service.getUserProfile$(this.uid) :
+            this.service.profile$;
+        this.profile$ = combineLatest([userProfile$, this.service.lokasi$]).pipe(switchMap(([profile, P]) => {
+            if (!profile) return of();
+            const profileDetails: ProfileDetails = {
+                profile,
+                uploads: [],
+                reviews: [],
+            };
+            for (const images of Object.values(profile.uploads)) {
+                for (const uploadRequest of Object.values(images)) {
+                    profileDetails.uploads.push({
+                        lokasi: P.getPrestineLokasi(uploadRequest.idLokasi.substring(0, 10)),
+                        uploadRequest
+                    });
+                }
+            }
+            profileDetails.uploads.sort((a, b) => {
+                return b.uploadRequest.votes[0].updateTs - a.uploadRequest.votes[0].updateTs;
+            });
+            for (const [idLokasi, numReviews] of Object.entries(profile.reviews)) {
+                profileDetails.reviews.push({
+                    idLokasi,
+                    lokasi: P.getPrestineLokasi(idLokasi.substring(0, 10)),
+                    numReviews
+                });
+            }
+            return of(profileDetails);
+        }));
     }
 }
