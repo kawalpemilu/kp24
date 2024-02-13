d341601166b2a76920c6fda5034f0d4ba99bb856
Zain Fathoni <me@zainf.dev>
2024-02-13 12:54:18 +0800

    feat(about): add expandable tutorial with more screenshots and video

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-about',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, MatExpansionModule, RouterLink],
     templateUrl: './about.component.html',
     styleUrl: './about.component.css'
 })
