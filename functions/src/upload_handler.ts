449ba5c009b6742d3363b851ddfc678d0b5d0286
Felix Halim <felix.halim@gmail.com>
2024-02-14 10:35:50 -0800

    Allow reviewer to approve photos in a locked TPS.

--- functions/src/upload_handler.ts
+++ functions/src/upload_handler.ts
@@ -30,13 +30,17 @@ function isIdentical(a: AggregateVotes, b: AggregateVotes): boolean {
 async function shouldLockTps(
   firestore: admin.firestore.Firestore,
   t: admin.firestore.Transaction,
-  tpsId: string) {
-  const tpsRef = firestore.collection(`t/${tpsId}/p`).where('status', '<', 2).limit(3);
+  data: UploadRequest  ) {
+  if (data.votes[0].status !== APPROVAL_STATUS.NEW) return false;
+  const tpsRef = firestore
+    .collection(`t/${data.idLokasi}/p`)
+    .where('status', '<', 2)
+    .limit(3);
   const snapshots = await t.get(tpsRef);
   let numNewOrApproved = 0;
   snapshots.forEach(x => { numNewOrApproved++; });
   if (numNewOrApproved >= 3) {
-    logger.error("Locked TPS", tpsId, numNewOrApproved);
+    logger.error("Locked TPS", data, numNewOrApproved);
     return true;
   }
   return false;
@@ -297,7 +301,7 @@ async function updateTps(firestore: admin.firestore.Firestore,
   const hRef = firestore.doc(`h/i${idDesa}`);
   return firestore
     .runTransaction(async (t) => {
-      if (await shouldLockTps(firestore, t, data.idLokasi)) return null;
+      if (await shouldLockTps(firestore, t, data)) return null;
       let lokasi = (await t.get(hRef)).data() as Lokasi | null;
       if (!lokasi) lokasi = LOKASI.getPrestineLokasi(idDesa);
       if (!lokasi) {
