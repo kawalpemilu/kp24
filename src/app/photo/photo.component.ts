4b59e9511be257a6e29423b20215b53f837fdc20
Shufi <shufisk@gmai.com>
2024-02-17 00:31:31 +0200

    layout UI review

--- src/app/photo/photo.component.ts
+++ src/app/photo/photo.component.ts
@@ -1,124 +1,143 @@
 import { Component, EventEmitter, Input, Output, Inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialog, MatDialogModule, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {
+  MatDialog,
+  MatDialogModule,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { NgxTippyModule, NgxTippyInstance } from 'ngx-tippy-wrapper';
 
 interface TippyInstanceWithLoading extends NgxTippyInstance {
-  _isFetching: boolean
-  _failedToLoad: boolean
+  _isFetching: boolean;
+  _failedToLoad: boolean;
 }
 
 @Component({
-    selector: 'app-photo',
-    standalone: true,
-    imports: [CommonModule, MatButtonModule, MatDialogModule, MatProgressSpinnerModule, NgxTippyModule],
-    templateUrl: './photo.component.html',
-    styleUrl: './photo.component.css',
+  selector: 'app-photo',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatProgressSpinnerModule,
+    NgxTippyModule,
+  ],
+  templateUrl: './photo.component.html',
+  styleUrl: './photo.component.css',
 })
 export class PhotoComponent {
-    @Input() maxWidth = 125;
-    @Input() maxHeight = 125;
-    @Input() hiResThumb = false;
-    @Input() showRoiTooltip = true;
-    @Output() roiImageUrl = new EventEmitter<string>();
+  @Input() maxWidth = 125;
+  @Input() maxHeight = 125;
+  @Input() hiResThumb = false;
+  @Input() showRoiTooltip = true;
+  @Output() roiImageUrl = new EventEmitter<string>();
+  @Output() roiImageUrl2 = new EventEmitter<string>();
 
-    largePhoto = '';
-    thumbnail = '';
-    tooltipUrl?: string = undefined;
+  largePhoto = '';
+  thumbnail = '';
+  tooltipUrl?: string = undefined;
 
-    @Input({ required: false })
-    set roiToolTip(value: string[]) {
-      this.tooltipUrl = this.roiUrl(value[0], value[1]);
-      this.roiImageUrl.emit(this.tooltipUrl);
+  @Input({ required: false })
+  set roiToolTip(value: string[]) {
+    this.tooltipUrl = this.roiUrl(value[0], value[1], 'paslon');
+    this.roiImageUrl.emit(this.tooltipUrl);
+    this.roiImageUrl2.emit(this.roiUrl(value[0], value[1], 'lokasi'));
+  }
+
+  roiUrl(
+    photoUrl: string | undefined,
+    tpsId: string,
+    type: 'paslon' | 'lokasi'
+  ) {
+    if (photoUrl == undefined || photoUrl.includes('data:image')) {
+      return '';
     }
 
-    roiUrl(photoUrl: string | undefined, tpsId: string) {
-      if (photoUrl == undefined || photoUrl.includes('data:image')) {
-        return ''
-      }
+    return !photoUrl
+      ? ''
+      : `https://storage.googleapis.com/kawalc1/static/2024/transformed/${tpsId.substring(
+          0,
+          10
+        )}/${tpsId.substring(10)}/extracted/${photoUrl.replace(
+          'http://lh3.googleusercontent.com/',
+          ''
+        )}%3Ds1280~${type}.webp`;
+  }
 
-      return !photoUrl ? '' :
-        `https://storage.googleapis.com/kawalc1/static/2024/transformed/${
-          tpsId.substring(0,10)}/${
-          tpsId.substring(10)}/extracted/${
-          photoUrl.replace('http://lh3.googleusercontent.com/', '')
-        }%3Ds1280~paslon.webp`
+  loadTooltipImage(ngxTippyInstance: NgxTippyInstance) {
+    const instance = ngxTippyInstance as TippyInstanceWithLoading;
+    instance._isFetching = true;
+    instance._failedToLoad = true;
+
+    const tooltipPicture = instance.reference.getAttribute('title');
+    if (!tooltipPicture || tooltipPicture == 'undefined') {
+      instance.disable();
+      return;
     }
-
-    loadTooltipImage(ngxTippyInstance: NgxTippyInstance) {
-      const instance = ngxTippyInstance as TippyInstanceWithLoading
-      instance._isFetching = true;
-      instance._failedToLoad = true;
-
-      const tooltipPicture = instance.reference.getAttribute('title')
-      if (!tooltipPicture || tooltipPicture == 'undefined') {
-        instance.disable()
-        return
-      }
-      fetch(tooltipPicture)
-        .then((response) => {
-          if (response.ok && response.status == 200) {
-            return response.blob()
-          }
-          instance._isFetching = true;
-          throw new Error(`${response.status} ${response.statusText}`);
-        })
-        .then((blob) => {
-          const src = URL.createObjectURL(blob);
-          const image = new Image();
-          image.style.display = 'block';
-          image.style.width = '150px';
-          image.src = src;
-          instance.setContent(image);
-          instance.props['maxWidth'] = '170px';
-          instance._failedToLoad = false;
-        })
-        .catch(() => {
-          instance.disable();
-          instance.setContent('');
-          instance._failedToLoad = true;
-        })
-        .finally(() => {
-          instance._isFetching = false;
-        });
-    }
-
-    onShowTippy(ngxTippyInstance: NgxTippyInstance) {
-      const instance = ngxTippyInstance as TippyInstanceWithLoading
-      if (instance._isFetching || instance._failedToLoad) {
-        return;
-      }
-    }
-
-    @Input({ required: true })
-    set photoUrl(value: string) {
-        let url = value;
-        if (value.startsWith("http://")) {
-            url = value.replace("http://", "https://");
+    fetch(tooltipPicture)
+      .then((response) => {
+        if (response.ok && response.status == 200) {
+          return response.blob();
         }
-        if (url.endsWith('.png') || url.startsWith('data:')) {
-            this.largePhoto = this.thumbnail = url;
-        } else {
-            this.largePhoto = url + '=s1280';
-            this.thumbnail = url + '=s200';
-        }
-    }
-
-    constructor(public dialog: MatDialog) {}
-
-    openDialog() {
-      this.dialog.open(PhotoDialog, {
-        height: '90vh',
-        width: '90vw',
-        data: {
-          largePhoto: this.largePhoto,
-        }
+        instance._isFetching = true;
+        throw new Error(`${response.status} ${response.statusText}`);
+      })
+      .then((blob) => {
+        const src = URL.createObjectURL(blob);
+        const image = new Image();
+        image.style.display = 'block';
+        image.style.width = '150px';
+        image.src = src;
+        instance.setContent(image);
+        instance.props['maxWidth'] = '170px';
+        instance._failedToLoad = false;
+      })
+      .catch(() => {
+        instance.disable();
+        instance.setContent('');
+        instance._failedToLoad = true;
+      })
+      .finally(() => {
+        instance._isFetching = false;
       });
+  }
 
-      return false;
+  onShowTippy(ngxTippyInstance: NgxTippyInstance) {
+    const instance = ngxTippyInstance as TippyInstanceWithLoading;
+    if (instance._isFetching || instance._failedToLoad) {
+      return;
     }
+  }
+
+  @Input({ required: true })
+  set photoUrl(value: string) {
+    let url = value;
+    if (value.startsWith('http://')) {
+      url = value.replace('http://', 'https://');
+    }
+    if (url.endsWith('.png') || url.startsWith('data:')) {
+      this.largePhoto = this.thumbnail = url;
+    } else {
+      this.largePhoto = url + '=s1280';
+      this.thumbnail = url + '=s200';
+    }
+  }
+
+  constructor(public dialog: MatDialog) {}
+
+  openDialog() {
+    this.dialog.open(PhotoDialog, {
+      height: '90vh',
+      width: '90vw',
+      data: {
+        largePhoto: this.largePhoto,
+      },
+    });
+
+    return false;
+  }
 }
 
 export interface PhotoDialogData {
--- src/app/photo/review.component.ts
+++ src/app/photo/review.component.ts
@@ -2,7 +2,13 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { APPROVAL_STATUS, ImageMetadata, PendingAggregateVotes, PrestineLokasi, Votes } from '../../../functions/src/interfaces';
+import {
+  APPROVAL_STATUS,
+  ImageMetadata,
+  PendingAggregateVotes,
+  PrestineLokasi,
+  Votes,
+} from '../../../functions/src/interfaces';
 import { AppService } from '../app.service';
 import { PhotoComponent } from './photo.component';
 import { DigitizeComponent } from '../upload/digitize.component';
@@ -10,143 +16,159 @@ import { FormsModule } from '@angular/forms';
 import { ProfileLinkComponent } from '../user/link.component';
 
 interface OptionLokasi {
-    id: string;
-    name: string;
+  id: string;
+  name: string;
 }
 
 @Component({
-    selector: 'app-review',
-    standalone: true,
-    imports: [CommonModule, MatButtonModule, FormsModule, ProfileLinkComponent,
-        MatProgressSpinnerModule, PhotoComponent, DigitizeComponent],
-    templateUrl: './review.component.html',
-    styleUrl: './review.component.css',
+  selector: 'app-review',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    FormsModule,
+    ProfileLinkComponent,
+    MatProgressSpinnerModule,
+    PhotoComponent,
+    DigitizeComponent,
+  ],
+  templateUrl: './review.component.html',
+  styleUrl: './review.component.css',
 })
 export class ReviewComponent {
-    @Input({required: true}) id!: string;
-    @Input({required: true}) votes!: Votes;
-    @Input({required: true}) imageId!: string;
-    @Input({required: true}) servingUrl!: string;
-    @Output() onReview = new EventEmitter<PendingAggregateVotes>();
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) votes!: Votes;
+  @Input({ required: true }) imageId!: string;
+  @Input({ required: true }) servingUrl!: string;
+  @Output() onReview = new EventEmitter<PendingAggregateVotes>();
 
-    ubahLokasi = false;
-    newId = '';
-    newPropId = '';
-    newKabId = '';
-    newKecId = '';
-    newKelId = '';
-    newTpsNo = '';
-    roiImageUrl = '';
+  ubahLokasi = false;
+  newId = '';
+  newPropId = '';
+  newKabId = '';
+  newKecId = '';
+  newKelId = '';
+  newTpsNo = '';
+  roiImageUrl = '';
+  roiImageUrl2 = '';
 
-    constructor(public service: AppService) { }
+  constructor(public service: AppService) {}
 
-    approve(votes: Votes) {
-        const onSubmitted = this.service
-            .review(this.id, this.imageId, votes)
-            .then(v => {
-                if (!v.data) {
-                    alert('Fail to submit the review');
-                    return '';
-                }
-                return 'ok';
-            }).catch(e => {
-                alert('Unable to review votes');
-                console.error('Unable review votes', e);
-                return '';
-            });
-        this.onReview.emit({
-            idLokasi: this.id,
-            name: '',
-            totalTps: 0,
-            totalPendingTps: 0,
-            totalErrorTps: 0,
-            totalCompletedTps: 0,
-            totalJagaTps: 0,
-            totalLaporTps: 0,
-            totalKpuTps: 0,
-            pas1: votes.pas1 || 0,
-            pas2: votes.pas2 || 0,
-            pas3: votes.pas3 || 0,
-            updateTs: 0,
-            status: votes.status,
-            uploadedPhoto: {
-                imageId: this.imageId,
-                photoUrl: this.servingUrl
-            },
-            onSubmitted
-        });
-    }
-
-    startUbahLokasi() {
-        this.ubahLokasi = true;
-        this.newPropId = this.id.substring(0, 2);
-        this.newKabId = this.id.substring(0, 4);
-        this.newKecId = this.id.substring(0, 6);
-        this.newKelId = this.id.substring(0, 10);
-        this.newTpsNo = this.id.substring(10);
-    }
-    sortIt(opts: OptionLokasi[]) {
-        return opts.sort((a, b) => a.name.localeCompare(b.name));
-    }
-    getPropinsiList(P: PrestineLokasi): OptionLokasi[] {
-        const aggregated = P.getPrestineLokasi('')?.aggregated || {};
-        return this.sortIt(Object.values(aggregated).map(a => {
-            return { id: a[0].idLokasi, name: a[0].name } as OptionLokasi;
-        }));
-    }
-    getKabupatenList(P: PrestineLokasi): OptionLokasi[] | undefined {
-        if (this.newPropId.length < 2) return undefined;
-        const aggregated = P.getPrestineLokasi(this.newPropId)?.aggregated || {};
-        return this.sortIt(Object.values(aggregated).map(a => {
-            return { id: a[0].idLokasi, name: a[0].name } as OptionLokasi;
-        }));
-    }
-    getKecamatanList(P: PrestineLokasi): OptionLokasi[] | undefined {
-        if (this.newKabId.length < 4) return undefined;
-        const aggregated = P.getPrestineLokasi(this.newKabId)?.aggregated || {};
-        return this.sortIt(Object.values(aggregated).map(a => {
-            return { id: a[0].idLokasi, name: a[0].name } as OptionLokasi;
-        }));
-    }
-    getKelurahanList(P: PrestineLokasi): OptionLokasi[] | undefined {
-        if (this.newKecId.length < 6) return undefined;
-        const aggregated = P.getPrestineLokasi(this.newKecId)?.aggregated || {};
-        return this.sortIt(Object.values(aggregated).map(a => {
-            return { id: a[0].idLokasi, name: a[0].name } as OptionLokasi;
-        }));
-    }
-    updateNewId(event: any, P: PrestineLokasi) {
-        this.newId = event.target.value;
-        if (this.newId.length <= 2) {
-            this.newPropId = this.newId;
-            this.newId = this.getKabupatenList(P)?.[0].id ?? '';
+  approve(votes: Votes) {
+    const onSubmitted = this.service
+      .review(this.id, this.imageId, votes)
+      .then((v) => {
+        if (!v.data) {
+          alert('Fail to submit the review');
+          return '';
         }
-        if (this.newId.length <= 4) {
-            this.newKabId = this.newId;
-            this.newId = this.getKecamatanList(P)?.[0].id ?? '';
-        }
-        if (this.newId.length <= 6) {
-            this.newKecId = this.newId;
-            this.newId = this.getKelurahanList(P)?.[0].id ?? '';
-        }
-        this.newKelId = this.newId;
+        return 'ok';
+      })
+      .catch((e) => {
+        alert('Unable to review votes');
+        console.error('Unable review votes', e);
+        return '';
+      });
+    this.onReview.emit({
+      idLokasi: this.id,
+      name: '',
+      totalTps: 0,
+      totalPendingTps: 0,
+      totalErrorTps: 0,
+      totalCompletedTps: 0,
+      totalJagaTps: 0,
+      totalLaporTps: 0,
+      totalKpuTps: 0,
+      pas1: votes.pas1 || 0,
+      pas2: votes.pas2 || 0,
+      pas3: votes.pas3 || 0,
+      updateTs: 0,
+      status: votes.status,
+      uploadedPhoto: {
+        imageId: this.imageId,
+        photoUrl: this.servingUrl,
+      },
+      onSubmitted,
+    });
+  }
+
+  startUbahLokasi() {
+    this.ubahLokasi = true;
+    this.newPropId = this.id.substring(0, 2);
+    this.newKabId = this.id.substring(0, 4);
+    this.newKecId = this.id.substring(0, 6);
+    this.newKelId = this.id.substring(0, 10);
+    this.newTpsNo = this.id.substring(10);
+  }
+  sortIt(opts: OptionLokasi[]) {
+    return opts.sort((a, b) => a.name.localeCompare(b.name));
+  }
+  getPropinsiList(P: PrestineLokasi): OptionLokasi[] {
+    const aggregated = P.getPrestineLokasi('')?.aggregated || {};
+    return this.sortIt(
+      Object.values(aggregated).map((a) => {
+        return { id: a[0].idLokasi, name: a[0].name } as OptionLokasi;
+      })
+    );
+  }
+  getKabupatenList(P: PrestineLokasi): OptionLokasi[] | undefined {
+    if (this.newPropId.length < 2) return undefined;
+    const aggregated = P.getPrestineLokasi(this.newPropId)?.aggregated || {};
+    return this.sortIt(
+      Object.values(aggregated).map((a) => {
+        return { id: a[0].idLokasi, name: a[0].name } as OptionLokasi;
+      })
+    );
+  }
+  getKecamatanList(P: PrestineLokasi): OptionLokasi[] | undefined {
+    if (this.newKabId.length < 4) return undefined;
+    const aggregated = P.getPrestineLokasi(this.newKabId)?.aggregated || {};
+    return this.sortIt(
+      Object.values(aggregated).map((a) => {
+        return { id: a[0].idLokasi, name: a[0].name } as OptionLokasi;
+      })
+    );
+  }
+  getKelurahanList(P: PrestineLokasi): OptionLokasi[] | undefined {
+    if (this.newKecId.length < 6) return undefined;
+    const aggregated = P.getPrestineLokasi(this.newKecId)?.aggregated || {};
+    return this.sortIt(
+      Object.values(aggregated).map((a) => {
+        return { id: a[0].idLokasi, name: a[0].name } as OptionLokasi;
+      })
+    );
+  }
+  updateNewId(event: any, P: PrestineLokasi) {
+    this.newId = event.target.value;
+    if (this.newId.length <= 2) {
+      this.newPropId = this.newId;
+      this.newId = this.getKabupatenList(P)?.[0].id ?? '';
     }
-    isValid(P: PrestineLokasi) {
-        if (this.newKelId.length !== 10) return false;
-        if (this.newTpsNo?.length <= 0) return false;
-        const lokasi = P.getPrestineLokasi(this.newKelId);
-        return !!lokasi?.aggregated[this.newTpsNo];
+    if (this.newId.length <= 4) {
+      this.newKabId = this.newId;
+      this.newId = this.getKecamatanList(P)?.[0].id ?? '';
     }
-    updateLokasi() {
-        this.service
-            .upload({
-                idLokasi: this.newKelId + this.newTpsNo,
-                imageId: this.imageId,
-                imageMetadata: {} as ImageMetadata,
-                servingUrl: this.servingUrl,
-                votes: [ { ...this.votes, status: APPROVAL_STATUS.MOVED } ],
-                status: APPROVAL_STATUS.MOVED,
-            });
-        this.approve({ ...this.votes, status: APPROVAL_STATUS.MOVED });
+    if (this.newId.length <= 6) {
+      this.newKecId = this.newId;
+      this.newId = this.getKelurahanList(P)?.[0].id ?? '';
     }
+    this.newKelId = this.newId;
+  }
+  isValid(P: PrestineLokasi) {
+    if (this.newKelId.length !== 10) return false;
+    if (this.newTpsNo?.length <= 0) return false;
+    const lokasi = P.getPrestineLokasi(this.newKelId);
+    return !!lokasi?.aggregated[this.newTpsNo];
+  }
+  updateLokasi() {
+    this.service.upload({
+      idLokasi: this.newKelId + this.newTpsNo,
+      imageId: this.imageId,
+      imageMetadata: {} as ImageMetadata,
+      servingUrl: this.servingUrl,
+      votes: [{ ...this.votes, status: APPROVAL_STATUS.MOVED }],
+      status: APPROVAL_STATUS.MOVED,
+    });
+    this.approve({ ...this.votes, status: APPROVAL_STATUS.MOVED });
+  }
 }
