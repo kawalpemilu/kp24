abdbd7ca2007807c55edbc248c04e75cdaae2154
Felix Halim <felix.halim@gmail.com>
2024-02-15 22:57:33 -0800

    Show link of the reviewer.

--- src/app/user/link.component.ts
+++ src/app/user/link.component.ts
@@ -6,9 +6,7 @@ import { RouterLink } from '@angular/router';
     selector: 'app-profile-link',
     standalone: true,
     imports: [CommonModule, RouterLink],
-    template: `<a [routerLink]="['/profile', uid]" target="_blank">
-        {{ uid.substring(0, 10) }}
-    </a>`,
+    template: `<a [routerLink]="['/profile', uid]" target="_blank">{{ uid.substring(0, 10) }}</a>`,
 })
 export class ProfileLinkComponent {
     @Input() uid = '';
